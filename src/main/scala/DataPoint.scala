31de6f177c73a48496b6fe66da0f659c94551c48
rupertbates <rupert.bates@guardian.co.uk>
2013-07-04 11:31:04 +0100

    Output chart.html

--- src/main/scala/DataPoint.scala
+++ src/main/scala/DataPoint.scala
@@ -1,10 +1,14 @@
 import org.joda.time.DateTime
 
 case class DataPoint(date : DateTime, dailyUniques : Int, notifications : List[PushNotification] = Nil) {
-  val comma = "\""
-  val delimiter = comma + "," + comma
+  val quote = "'"
+  val delimiter = ","
   override def toString: String = {
-    val headlines = notifications.foldLeft("")((acc, v) => acc + v.message + ",")
-    comma + date.toString("d/MMM/yyyy") + delimiter + dailyUniques + delimiter + headlines.take(50) + comma
+    val headlines = notifications.foldLeft("")((acc, v) => acc + v.message + "\\n")
+    val output = headlines.take(headlines.size - "\\n".length
+    ).replace("'", "\\'")
+    val noHyphens = output.replace("–", ":")
+    println(output)
+    "[" + quote + date.toString("d/MMMM/yyyy") + quote + delimiter + dailyUniques + delimiter + quote + noHyphens + quote + "],"
   }
 }
--- src/main/scala/ParserApp.scala
+++ src/main/scala/ParserApp.scala
@@ -1,13 +1,16 @@
 import java.io.File
+import org.joda.time.DateTime
 import org.joda.time.format.DateTimeFormat
 import scala.io.Source
 import Helpers._
+
 
 object ParserApp extends App {
 
   val flurryPattern = DateTimeFormat.forPattern("yyyy/MM/dd HH:mm")
   val pushyPattern = DateTimeFormat.forPattern("EEEE, d MMMM yyyy - HH:mm:ss")
-
+  val liveDate = new DateTime(2013, 1, 23, 0, 0, 0) //date the service went live to real users
+  val noPushes = List(new PushNotification(DateTime.now(), "No push sent"))
   override def main(args: Array[String]) {
     super.main(args)
 
@@ -20,7 +23,8 @@ object ParserApp extends App {
       val tokens = CsvLine(line)
       val date = pushyPattern.parseDateTime(tokens.token(0)).toDateMidnight.toDateTime
       PushNotification(date, tokens.token(1))
-    }
+    }.filter(p => p.date.isAfter(liveDate))
+
     val distinct = pushes.distinctBy(_.message)
     val grouped = distinct groupBy(_.date)
 
@@ -29,19 +33,22 @@ object ParserApp extends App {
     val dataPoints = Source.fromFile("data/AndroidNews-2013_07_01-activeUsers.csv")
       .getLines()
       .filter(!_.startsWith("Incomplete"))
+      .toList
+      .reverse
       .map {
       line =>
         val tokens = CsvLine(line)
         val date = flurryPattern.parseDateTime(tokens.token(1)).toDateMidnight.toDateTime()
-        val pushes = grouped.getOrElse(date, Nil)
+        val pushes = grouped.getOrElse(date, noPushes)
         DataPoint(date, tokens.token(2).toInt, pushes)
     }
 
-    val output = new File("output.csv")
+    val output = new File("chart.html")
     if (output.exists()) output.delete()
     printToFile(output)(p => {
-
+      p.println(Templates.head)
       dataPoints.foreach(p.println)
+      p.println(Templates.tail)
     })
 
   }
